714828a665f1a38fbfae6f3aef91beed5986af3c
bram <nogrend@msn.com>
2025-02-23 10:36:38 +0100

    template login stuff

---
+++ login.h
@@ -0,0 +1,16 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+inline auto deviceId = "";
+
+inline auto ssid = "";
+inline auto password = "";
+
+inline auto mqttUsername = "";
+inline auto mqttPassword = "";
+inline auto mqttPort = 0;
+inline auto mqttServer = "";
+inline auto mqttTopic = "";
+
+#endif //LOGIN_H
+
